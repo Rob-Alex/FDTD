e534c17ea1d69447debc29d00556655358a63227
Rob <rob.alexander.115@gmail.com>
2023-12-18 22:32:13 +0000

    Initial Commit

---
+++ FDTD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FDTD
+//
+//  Created by Robbie Alexander on 18/12/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
