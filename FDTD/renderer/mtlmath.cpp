9f273d1f4714b45abdd01d6b953395444a5b1257
Rob <rob.alexander.115@gmail.com>
2024-01-07 23:59:44 +0000

    added simple transforms

---
+++ FDTD/renderer/mtlmath.cpp
@@ -0,0 +1,8 @@
+//
+//  mtlmath.cpp
+//  FDTD
+//
+//  Created by Robbie Alexander on 07/01/2024.
+//
+
+#include "mtlmath.hpp"
---
+++ FDTD/renderer/mtlmath.hpp
@@ -0,0 +1,13 @@
+//
+//  mtlmath.hpp
+//  FDTD
+//
+//  Created by Robbie Alexander on 07/01/2024.
+//
+
+#ifndef mtlmath_hpp
+#define mtlmath_hpp
+
+#include <stdio.h>
+
+#endif /* mtlmath_hpp */
